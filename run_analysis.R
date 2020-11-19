ed95a40d73d34d6980840366fafcb174f3cfd1a5
Diego Martínez <dialm170@gmail.com>
2020-11-19 12:28:24 -0600

    Reformat

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 library("reshape2")
 
-
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","data.zip")
 unzip("data.zip")
 
@@ -13,14 +12,11 @@ yTrain <- read.table("UCI HAR Dataset/train/y_train.txt")
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
 
-# 1. 
 dataSet <- rbind(XTrain,XTest)
 
-# 2. 
 MeanSt <- grep("mean()|std()", features[, 2]) 
 dataSet <- dataSet[,MeanSt]
 
-# 4. 
 featuresNames <- sapply(features[, 2], function(x) {gsub("[()]", "",x)})
 names(dataSet) <- featuresNames[MeanSt]
 
@@ -31,12 +27,10 @@ names(activity) <- 'activity'
 
 dataSet <- cbind(subject,activity, dataSet)
 
-# 3. 
 groupActivity <- factor(dataSet$activity)
 levels(groupActivity) <- activityLabels[,2]
 dataSet$activity <- groupActivity
 
-# 5. 
 baseData <- melt(dataSet,(id.vars=c("subject","activity")))
 secondDataSet <- dcast(baseData, subject + activity ~ variable, mean)
 names(secondDataSet)[-c(1:2)] <- paste("[mean of]" , names(secondDataSet)[-c(1:2)] )
